017670e4abf362951f7c9ec0394b8ea5faa60062
Rob Steele <Robert.D.Steele@jpl.nasa.gov>
2018-12-08 14:57:04 -0800

    Still having problems with setting long exposures - but the crashing seems to have gone away. Added an exept clause to the try case.

--- captureImages.py
+++ captureImages.py
@@ -3,7 +3,12 @@ from fractions import Fraction
 import picamera
 with picamera.PiCamera() as camera:
 
+    # Looks like this does the open included in the logic:
+    
     camera.start_preview()
+    camera.exposure_mode = 'off'
+
+    # Print basic information:
     
     print 'framerate     : ', camera.framerate
     print 'shutter speed : ', camera.shutter_speed
@@ -26,42 +31,55 @@ with picamera.PiCamera() as camera:
 
 # Changing resolution to 1640x1232 for telescope
 
+    print 'Enter shutter speed 0.000001 to 2.0 seconds)'
+    shutterSpeedSeconds  = input('Shutter speed (seconds) : ')
+    
+    camera.shutter_speed = int(shutterSpeedSeconds * 1000000)
+
+    print 'Enter ISO, 100, 200, 300, ..., 800'
+    camera.iso = input('ISO: ')
+
+    numberOfImages = input('Enter number of images to grab : ')
+    
     camera.resolution  = (1640,1232)
     camera.sensor_mode = 4
-    camera.framerate   = Fraction (1,5)
+
+    # Set framerate based on the shutter speed.
+
+    newFrameRate = 2.0 * shutterSpeedSeconds
+    print 'newFrameRate: ', newFrameRate
+    
+    if newFrameRate < 0.250:
+        camera.framerate = Fraction(2,1)
+    elif newFrameRate < 0.500:
+        camera.framerate = Fraction(1,1)
+    elif newFrameRate < 1.0:
+        camera.framerate = Fraction(1,2)
+    elif newFrameRate < 2.5:
+        camera.framerate = Fraction(1,5)
+    elif newFrameRate < 5.0:
+        camera.framerate = Fraction(1,10)
+
+    print 'Wait 20 seconds to reset camera.'
+    sleep(20)
     
 # Mode 4 for the V2 will do a 2x2 binning, a resolution of 1640x1233,
 #        support framerates of 1/10 to 15 frames per seconds,
 #        and uses the entire frame.
 
-#    print 'Framerate : ', camera.framerate
-
-#    print 'Enter shutter speed 0.000001 to 2.0 seconds)'
-#    camera.shutter_speed = int(input('Shutter speed (seconds): ') * 1000000)
-
                            
-# worked but overexposed during the day: camera.shutter_speed = 5000000
-# camera.shutter_speed = 100000
-
-# worked but overexposed during the day: camera.iso = 800
-
-#    print 'Enter ISO, 100, 200, 300, ..., 800'
-
-#    camera.iso = input('ISO: ')
-
-    numberOfImages = input('Enter number of images to grab : ')
-
 # Give the camera a good long time to set gains and
 # measure AWB (you may wish to use fixed AWB instead)
 #
-# camera.exposure_mode = 'off'
 
-    print 'framerate     : ', camera.framerate
-    print 'shutter speed : ', camera.shutter_speed
-    print 'iso           : ', camera.iso
-    print 'exposure_mode : ', camera.exposure_mode
-    print 'resolution    : ', camera.resolution
-    print 'sensor_mode   : ', camera.sensor_mode
+    print 'framerate           : ', camera.framerate
+    print 'framerate           : ', float(camera.framerate)
+    print 'shutterSpeedSeconds : ', shutterSpeedSeconds
+    print 'shutter speed       : ', camera.shutter_speed/1000000.0
+    print 'iso                 : ', camera.iso
+    print 'exposure_mode       : ', camera.exposure_mode
+    print 'resolution          : ', camera.resolution
+    print 'sensor_mode         : ', camera.sensor_mode
 
 # worked but will try a smaller number: sleep(30)
 #    print 'sleep 10 seconds'
@@ -79,8 +97,14 @@ with picamera.PiCamera() as camera:
             print(filename)
             if i == numberOfImages-1:
                 break
+    except:
+        print 'except section hit'
+        print 'call stop_preview'
+        camera.stop_preview()
     finally:
         print 'finally section hit'
+        print 'call stop_preview'
+        camera.stop_preview()
         # print 'Close camera'
         # camera.close()
 
